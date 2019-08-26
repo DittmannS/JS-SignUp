f91713065d2c3eec1cc706db369b0bae6524a907
dittmanns <dittmanns@ergovia.de>
2019-08-26 14:59:50 +0200

    - frontend hinzugefügt

--- signUp/app.js
+++ signUp/app.js
@@ -5,16 +5,27 @@ Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
 const users = require('./routes/users');
 const auth = require('./routes/auth');
+const path = require('path');
 const express = require('express');
 const app = express();
+
+const indexRouter = require('./routes/index');
 
 mongoose.connect('mongodb://localhost/mongo-games')
 .then(() => console.log('Now connected to MongoDB!'))
 .catch(err => console.error('Something went wrong', err));
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+
 app.use(express.json());
 app.use('/api/users', users);
 app.use('/api/auth', auth);
+
+app.use('/', indexRouter);
 
 const port = process.env.PORT || 4001;
 app.listen(port, () => console.log(`Listening in port ${port}...`));
